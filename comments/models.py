57b144ce943dfbca256a67a6f077485e7f210750
sudog <sjin0704@gmail.com>
2023-09-14 05:50:09 +0900

    add comment feature

---
+++ comments/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from accounts.models import UserModel
+from code_feed.models import CodeModel
+
+
+# Create your models here.
+class CommentModel(models.Model):
+    author = models.ForeignKey(
+        UserModel, on_delete=models.CASCADE, related_name="comments"
+    )
+    post = models.ForeignKey(
+        CodeModel, on_delete=models.CASCADE, related_name="comments"
+    )
+    content = models.TextField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
